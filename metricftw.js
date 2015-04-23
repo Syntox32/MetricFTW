8e0b6f565e3d0141b7876f594a8dc53dd5414ffa
Syntox32 <syntox32@gmail.com>
2015-04-23 23:15:40 +0200

    namespaces or something

--- metricftw.js
+++ metricftw.js
@@ -1,102 +1,101 @@
 
-function removeWhitespace(string) {
-	return string.replace(/ /g, "");
-}
+var MetricFTW = MetricFTW || {
 
-function parseNumeric(numString) {
-	var re = /[\\ \\.\\,]+/gi;
-	numString = numString.replace(re, ""); // parseInt doesn't remove ','
-	return parseInt(numString);
-}
+    // http://james.padolsey.com/javascript/find-and-replace-text-with-javascript/
+    filterImperial: function(searchNode) {
+        var valueReg = "(\\d[0-9\\/\\,\\.\\s]+\\s*)";
+        var nameReg= "(pounds|lbs|lb)";
+        var reg = new RegExp(valueReg + nameReg, "gi");
 
-/**
- * http://www.initium.demon.co.uk/converts/metimp.htm
- */
-function convertToMetric(match) {
-	name = removeWhitespace(match[2].toLowerCase());
-	value = parseNumeric(match[1]);
-	var newName = "" , newValue = 0;
+        var childNodes = (searchNode || document.body).childNodes,
+            excludes = 'html,head,style,title,link,meta,script,object,iframe',
+            cnLength = childNodes.length,
+            peasantryFound = 0;
 
-	switch(name) {
-		case "pounds":
-		case "lbs":
-		case "lb":
-			newName = "kg";
-			newValue = peasant_table["pounds"] * value;
-			break;
-		default:
-			newName = "unknown";
-			newValue = "-1";
-			break;
-	}
+        while (cnLength--) {
+            var currentNode = childNodes[cnLength];
+            if (currentNode.nodeType === 1 
+                && (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
+                peasantryFound += arguments.callee(currentNode);
+            }
 
-	return {
-		name: newName,
-		value: newValue
-	}
-}
+            m = reg.exec(currentNode.data);
+            if (currentNode.nodeType !== 3 || m === null) {
+                continue;
+            }
 
-/**
- * Original code from http://james.padolsey.com/javascript/find-and-replace-text-with-javascript/
- */
-function filterImperial(searchNode) {
-	var valueReg = "(\\d[0-9\\/\\,\\.\\s]+\\s*)";
-	var nameReg= "(pounds|lbs|lb)";
-	var reg = new RegExp(valueReg + nameReg, "gi");
+            // reset regex index when getting multiple matches
+            reg.lastIndex = 0;
+            
+            peasantryFound++;
+            metric = MetricFTW.convertToMetric(m);
 
-    var childNodes = (searchNode || document.body).childNodes,
-	 	excludes = 'html,head,style,title,link,meta,script,object,iframe',
-    	cnLength = childNodes.length,
-    	peasantryFound = 0;
+            // I'll add a star cause I'll forget I have it turned on if it changes something
+            replacement = metric.value.toString() + " " + metric.name + "*";
 
-    while (cnLength--) {
-        var currentNode = childNodes[cnLength];
-        if (currentNode.nodeType === 1 
-        	&& (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
-            peasantryFound += arguments.callee(currentNode);
+            // replace the peasantry with metric values
+            var parent = currentNode.parentNode,
+                frag = (function() {
+                    var html = currentNode.data.replace(reg, replacement),
+                        wrap = document.createElement('div'),
+                        frag = document.createDocumentFragment();
+                    wrap.innerHTML = html;
+                    while (wrap.firstChild) {
+                        frag.appendChild(wrap.firstChild);
+                    }
+                    return frag;
+                })();
+            parent.insertBefore(frag, currentNode);
+            parent.removeChild(currentNode);
+        }
+        return peasantryFound;
+    },
+
+    // http://www.initium.demon.co.uk/converts/metimp.htm
+    convertToMetric: function(match) {
+        name = this.removeWhitespace(match[2].toLowerCase());
+        value = this.parseNumeric(match[1]);
+        var newName = "" , newValue = 0;
+
+        switch(name) {
+            case "pounds":
+            case "lbs":
+            case "lb":
+                newName = "kg";
+                newValue = this.peasant_table["pounds"] * value;
+                break;
+            default:
+                newName = "unknown";
+                newValue = "-1";
+                break;
         }
 
-        m = reg.exec(currentNode.data);
-        if (currentNode.nodeType !== 3 || m === null) {
-            continue;
+        return {
+            name: newName,
+            value: newValue
         }
-        // reset regex index when getting multiple matches
-        reg.lastIndex = 0;
-        peasantryFound++;
+    },
+        
+    removeWhitespace: function(string) {
+        return string.replace(/ /g, "");
+    },
 
-        metric = convertToMetric(m);
+    parseNumeric: function(numString) {
+        var re = /[\\ \\.\\,]+/gi;
+        numString = numString.replace(re, ""); // parseInt doesn't remove ','
+        return parseInt(numString);
+    },
 
-        // I'll add a star cause I'll forget I have it turned on if it changes something
-        replacement = metric.value.toString() + " " + metric.name + "*";
-
-        // replace the peasantry with metric values
-        var parent = currentNode.parentNode,
-            frag = (function() {
-                var html = currentNode.data.replace(reg, replacement),
-                    wrap = document.createElement('div'),
-                    frag = document.createDocumentFragment();
-                wrap.innerHTML = html;
-                while (wrap.firstChild) {
-                    frag.appendChild(wrap.firstChild);
-                }
-                return frag;
-            })();
-        parent.insertBefore(frag, currentNode);
-        parent.removeChild(currentNode);
+    peasant_table: {
+        pounds: 0.4536
     }
-
-    return peasantryFound;
 }
 
-peasant_table = {
-	pounds: 0.4536,
+var init = function() {
+    var numActs =  MetricFTW.filterImperial();
+    console.log("Removed " + numActs.toString() + " act(s) of peasantry!");
+    return numActs;
 };
-
-function init() {
-	var acts = filterImperial();
-	console.log("Removed " + acts.toString() + " act(s) of peasantry!");
-    return acts;
-}
 
 chrome.extension.sendMessage({
     action: "filterImperial",
